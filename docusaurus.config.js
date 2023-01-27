540b59ff4f16d1c570e36f4b86e4308d8a314e81
Hedgemon4 <sethlinc4@gmail.com>
2023-01-27 11:28:00 -0800

    Changed website title and configured navbar and tutorial button to lead to documentation overview instead

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'My Site',
-    tagline: 'Dinosaurs are cool',
+    title: 'Canvas Gamification JUnit Documentation',
+    tagline: 'Documentation is cool',
     url: 'https://your-docusaurus-test-site.com',
     baseUrl: '/docs/',
     onBrokenLinks: 'throw',
@@ -43,7 +43,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
         ({
             navbar: {
-                title: 'My Site',
+                title: 'Canvas Gamification JUnit Documentation',
                 logo: {
                     alt: 'My Site Logo',
                     src: 'img/logo.svg',
@@ -51,11 +51,10 @@ const config = {
                 items: [
                     {
                         type: 'doc',
-                        docId: 'intro',
+                        docId: 'junit-framework/introduction/overview',
                         position: 'left',
-                        label: 'Tutorial',
+                        label: 'Documentation',
                     },
-                    {to: '/blog', label: 'Blog', position: 'left'},
                     {
                         href: 'https://github.com/facebook/docusaurus',
                         label: 'GitHub',
